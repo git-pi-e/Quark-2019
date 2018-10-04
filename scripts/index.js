eb17840d479871f820fdcbbb9c0a1dcdbb14f870
Overlord720 <31623420+Overlord720@users.noreply.github.com>
2018-10-04 21:57:53 +0530

    Backend complete

--- scripts/index.js
+++ scripts/index.js
@@ -8,18 +8,25 @@ $(function () {
         regData.placeOfCollege = $("#CollegePlace").val();
         regData.emailOfPerson = $("#Email").val();
         regData.phoneNoOfPerson = $("#PhoneNumber").val();
-        regData.referralCode = "101";
+        regData.referralCode = $("#ReferralCode").val();;
         regData.eventsRegistered = [];    
         for (let index = 0; index < 24; index++) {
-            console.log($("#evntchk-"+index).is(':checked'));
+            // console.log($("#evntchk-"+index).is(':checked'));
             if( $("#evntchk-"+index).is(':checked') === true) {
                 regData.eventsRegistered.push($("#label-evntchk-"+index).text());
             }            
         }
+        console.log(regData);
         const REGISTRATION_PATH = "/main/registrations/";
         var dbRef = firebase.database().ref(REGISTRATION_PATH);
         var newKey = dbRef.push().key;
-        dbRef.child(newKey).set(regData);
+        dbRef.child(newKey).set(regData).then(function (error) {
+            if(error)
+               toastr.error(error);
+            else{
+                toastr.success("Success");
+            }
+        });
 
     });
 
