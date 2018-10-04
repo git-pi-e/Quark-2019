cceab10c88058625a0968704dd158df85b161966
Overlord720 <31623420+Overlord720@users.noreply.github.com>
2018-10-04 20:01:37 +0530

    Form changed

---
+++ scripts/firebase_init.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBTHW4G-Oblrq6N_CNjH8UbKuPLYWYWuzQ",
+    authDomain: "dummy-72d32.firebaseapp.com",
+    databaseURL: "https://dummy-72d32.firebaseio.com",
+    projectId: "dummy-72d32",
+    storageBucket: "dummy-72d32.appspot.com",
+    messagingSenderId: "381810700829"
+  };
+  firebase.initializeApp(config);
